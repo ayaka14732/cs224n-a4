b9cc0af4bc33ff8231b3c785e61689f3fe2e20a3
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-03-01 14:58:33 +0800

    Fix bug

--- nmt_model.py
+++ nmt_model.py
@@ -340,7 +340,7 @@ class NMT(nn.Module):
         ###         https://pytorch.org/docs/stable/torch.html#torch.cat
         ###     Tanh:
         ###         https://pytorch.org/docs/stable/torch.html#torch.tanh
-        alpha_t = F.softmax(e_t)
+        alpha_t = F.softmax(e_t, dim=1)
         a_t = torch.bmm(alpha_t.unsqueeze(1), enc_hiddens).squeeze(1)
         U_t = torch.cat((dec_hidden, a_t), 1)
         V_t = self.combined_output_projection(U_t)
